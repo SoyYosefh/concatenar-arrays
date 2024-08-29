9f888acfc04c6a1455f07db89e638ab3e296291a
SoyYosefh <joratejedana@gmail.com>
2024-08-28 18:13:44 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// 7. Concatenación de arreglos: Crea dos arreglos de strings 
+// y luego concatenarlos en un solo arreglo.
+
+const colores1 = ['rojo', 'verde', 'azul'];
+
+const colores2 = ['amarillo', 'naranja', 'violeta'];
+
+const colores = colores1.concat(colores2);
+
+console.log(`Colores 1: ${colores1}`);
+console.log(`Colores 2: ${colores2}`);
+console.log(`Colores concatenados: ${colores}`);
